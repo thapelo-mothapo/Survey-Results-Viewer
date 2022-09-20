ebc163665fe20814014f7a833e06c3347c8f0827
Thapelo <dev.mothapotr@gmail.com>
2022-09-20 21:47:12 +0200

    modify and style root page

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import Router from "./routes";
+import "./index.css";
+import SideNav from "./components/navigation";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const App = () => {
+  return (
+    <div className="page-container">
+      <SideNav />
+      <Router />
+    </div>
+  );
+};
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
